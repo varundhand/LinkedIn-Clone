bbe93d9876067018fad56b8818bd99481cc3d6a0
Varun Dhand <varundhand77@gmail.com>
2024-05-19 19:48:47 +0530

    Feat: Define instance and static methods on posts model

--- mongodb/models/post.ts
+++ mongodb/models/post.ts
@@ -1,6 +1,6 @@
 import mongoose, {Schema, Document, models, Model} from "mongoose";
 import { IUser } from "@/types/user";
-import { IComment } from "@/types/comments";
+import { IComment, ICommentBase } from "@/types/comments";
 
 export interface PostBaseInterface {
     user: IUser,
@@ -15,3 +15,19 @@ export interface IPost extends PostBaseInterface, Document {
     updatedAt: Date,
 }
 
+// Instance Method: perform operations on individual documents
+// Static Method: perform operations on the entire collection
+interface IPostMethods {
+    likedPost(userId: string): Promise<void>;
+    unlikePost(userId: string): Promise<void>;
+    commentOnPost (comment: ICommentBase): Promise<void>;
+    getAllComments(): Promise<IComment[]>;
+    removePost(): Promise<void>;
+}
+
+interface IPostStatics {
+    getAllPosts(): Promise<IPostDocument[]>;
+}
+
+export interface IPostDocument extends IPost, IPostMethods {} // singular instance or post 
+interface IPostModel extends Model<IPostDocument>, IPostStatics {} // entire collection of posts
